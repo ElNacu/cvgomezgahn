3be9e9093277f7734cb99da6d9536969cfb4a013
ElNacu <124333449+ElNacu@users.noreply.github.com>
2023-02-03 02:03:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 function display_texto(idtexto){
-    /*if (idtexto==1){
-        const elemento=document.getElementsByClassName("desc_general");
-        const boton=document.getElementsByClassName("btn1");
-        if (elemento[0].style.display=="block"){
-            elemento[0].style.display="none"; boton[0].style.border="0px"}
-        else {elemento[0].style.display="block"; boton[0].style.border="3px solid grey"}
-    }
-    if (idtexto==2){
-        const elemento=document.getElementsByClassName("formacion");
-        const boton=document.getElementsByClassName("btn2");
-        if (elemento[0].style.display=="block"){
-            elemento[0].style.display="none"; boton[0].style.border="0px"}
-        else {elemento[0].style.display="block"; boton[0].style.border="3px solid grey"}
-    }
-    if (idtexto==3){
-        const elemento=document.getElementsByClassName("exp");
-        const boton=document.getElementsByClassName("btn3");
-        if (elemento[0].style.display=="block"){
-            elemento[0].style.display="none"; boton[0].style.border="0px"}
-        else {elemento[0].style.display="block"; boton[0].style.border="3px solid grey"}
-    }
-    if (idtexto==4){
-        const elemento=document.getElementsByClassName("conoc");
-        const boton=document.getElementsByClassName("btn4");
-        if (elemento[0].style.display=="block"){
-            elemento[0].style.display="none"; boton[0].style.border="0px"}
-        else {elemento[0].style.display="block"; boton[0].style.border="3px solid grey"}
-    }*/
     const elemento=document.getElementsByClassName("info");
     const boton=document.getElementsByClassName("boton");
     if (elemento[idtexto-1].style.display=="block"){
